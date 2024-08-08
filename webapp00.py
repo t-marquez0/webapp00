17b06ec4698e93e0f73c11974253933907aad7ed
t-marquez0 <thainaracristinamar@gmail.com>
2024-08-08 09:52:44 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,7 +3,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.title("Olá, gostaria?")
-st.header("Turma 2EI-S")
-st.subheader("Carne de batata com panela")
-st.write("quero almoçar!")
+st.title("Olá, gostaria")
+st.header("que apagasse a minha foto")
+st.subheader("ainda mais pq se trata de uma montagem")
+st.write("oxi nn é montagem n mor, é apenas um repost")
