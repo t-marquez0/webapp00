0649ba745b4f727e0bb27c05485c3ebec37d31e3
t-marquez0 <thainaracristinamar@gmail.com>
2024-08-08 09:51:17 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,7 +3,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.title("Olá, Sophia!")
+st.title("Olá, linda!")
 st.header("Turma 2EI-S")
 st.subheader("Carne de batata com panela")
 st.write("quero almoçar!")
